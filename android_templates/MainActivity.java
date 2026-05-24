634d04431838228610f58b3ea85c9be88ab5d342
barq <barqaday@gmail.com>
2026-05-24 20:23:59 +0300

    Update MainActivity.java

--- android_templates/MainActivity.java
+++ android_templates/MainActivity.java
@@ -1,22 +1,15 @@
-package com.joud.chat;
+package com.dardshati.app;
 
-import com.getcapacitor.BridgeActivity;
 import android.os.Bundle;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
-import com.codetrixstudio.capacitor.GoogleAuth.GoogleAuth;
+import com.getcapacitor.BridgeActivity;
+import com.codetrixstudio.capacitorGoogleAuth.GoogleAuth;
 
 public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        registerPlugin(GoogleAuth.class);
         super.onCreate(savedInstanceState);
-        
-        WebView webView = getBridge().getWebView();
-        if (webView != null) {
-            WebSettings settings = webView.getSettings();
-            settings.setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
-            settings.setDomStorageEnabled(true);
-        }
+
+        // تفعيل جوجل Auth
+        registerPlugin(GoogleAuth.class);
     }
 }
