3652e6e02da417403ef748c3c5c43d900883ce2b
barq <barqaday@gmail.com>
2026-05-24 16:52:20 +0300

    Create capacitor.config.ts

---
+++ src/capacitor.config.ts
@@ -0,0 +1,22 @@
+import type { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "com.dardshati.app", // تأكد من مطابقة هذا لاسم الحزمة في Firebase تماماً
+  appName: "دردشة",
+  webDir: "dist",
+  server: {
+    androidScheme: "https",
+  },
+  plugins: {
+    GoogleAuth: {
+      scopes: ["profile", "email"],
+      // استخدم نفس الـ Client ID الخاص بـ ANDROID
+      androidClientId: "159537926588-dtnce28t16apvq1calgdvjj1u1t95iq3.apps.googleusercontent.com",
+      // الـ serverClientId هو الخاص بالويب/الخلفية (Supabase)
+      serverClientId: "62134907551-t9cac5jbefl8o6pl1epbae1p8dscipj8.apps.googleusercontent.com",
+      forceCodeForRefreshToken: true,
+    },
+  },
+};
+
+export default config;
