14ae4f60c8a69d152ebf7760f1b52730f3f029ea
barq <barqaday@gmail.com>
2026-05-24 16:36:18 +0300

    Create secrets.ts

---
+++ secrets.ts
@@ -0,0 +1,6 @@
+{
+  "GOOGLE_CLIENT_ID_ANDROID": "159537926588-dtnce28t16apvq1calgdvjj1u1t95iq3.apps.googleusercontent.com",
+  "GOOGLE_CLIENT_ID_WEB": "62134907551-t9cac5jbefl8o6pl1epbae1p8dscipj8.apps.googleusercontent.com",
+  "SIGNING_CONFIG_ENABLED": true
+}
+
