c0a620c2c906443487165fed50aee2c46245ae87
barq <barqaday@gmail.com>
2026-05-26 01:59:39 +0300

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: "com.drdshati.app",
+  appId: "com.dardshati.app",
   appName: "دردشة",
   webDir: "dist",
   server: {
