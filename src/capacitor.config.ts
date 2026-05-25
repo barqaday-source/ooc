730a8179d80c664fe6ed0c6ea3e9b3cb9da23097
barq <barqaday@gmail.com>
2026-05-26 00:51:57 +0300

    Update capacitor.config.ts

--- src/capacitor.config.ts
+++ src/capacitor.config.ts
@@ -1,21 +1,56 @@
-import type { CapacitorConfig } from "@capacitor/cli";
+import { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: "com.dardshati.app", // تأكد من مطابقة هذا لاسم الحزمة في Firebase تماماً
+  appId: "com.drdshati.app",
   appName: "دردشة",
   webDir: "dist",
   server: {
     androidScheme: "https",
+    cleartext: true,
+  },
+  android: {
+    backgroundColor: "#B6D6FF",
+    allowMixedContent: true,
+    captureInput: true,
+    webContentsDebuggingEnabled: false,
+  },
+  ios: {
+    backgroundColor: "#B6D6FF",
+    contentInset: "always",
+    scrollEnabled: false,
+    limitsNavigationsToAppBoundDomains: true
   },
   plugins: {
+    CapacitorHttp: {
+      enabled: true
+    },
     GoogleAuth: {
       scopes: ["profile", "email"],
-      // استخدم نفس الـ Client ID الخاص بـ ANDROID
       androidClientId: "159537926588-dtnce28t16apvq1calgdvjj1u1t95iq3.apps.googleusercontent.com",
-      // الـ serverClientId هو الخاص بالويب/الخلفية (Supabase)
-      serverClientId: "62134907551-t9cac5jbefl8o6pl1epbae1p8dscipj8.apps.googleusercontent.com",
+      serverClientId: "159537926588-dtnce28t16apvq1calgdvjj1u1t95iq3.apps.googleusercontent.com",
       forceCodeForRefreshToken: true,
     },
+    StatusBar: {
+      style: "dark",
+      backgroundColor: "#B6D6FF",
+      overlaysWebView: false,
+      androidOverlaysWebView: false
+    },
+    Keyboard: {
+      resize: "body",
+      style: "dark",
+      resizeOnFullScreen: true,
+    },
+    SplashScreen: {
+      launchShowDuration: 0,
+      launchAutoHide: true,
+      backgroundColor: "#B6D6FF",
+      androidSplashResourceName: "splash",
+      androidScaleType: "CENTER_CROP",
+      showSpinner: false,
+      splashFullScreen: false,
+      splashImmersive: false,
+    }
   },
 };
 
