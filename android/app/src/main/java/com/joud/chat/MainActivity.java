7078545ace96ba720a8bb00504168e7085c83d32
barq <barqaday@gmail.com>
2026-05-25 16:41:26 +0300

    Update MainActivity.java

--- android/app/src/main/java/com/joud/chat/MainActivity.java
+++ android/app/src/main/java/com/joud/chat/MainActivity.java
@@ -1,5 +1,17 @@
-package com.joud.chat;
+package com.yourpackage
 
-import com.getcapacitor.BridgeActivity;
+import android.os.Bundle
+import com.getcapacitor.BridgeActivity
 
-public class MainActivity extends BridgeActivity {}
+class MainActivity : BridgeActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        
+        // منع الـ WebView من الـ scroll والـ over-scroll
+        this.bridge.webView.apply {
+            isVerticalScrollBarEnabled = false
+            isHorizontalScrollBarEnabled = false
+            overScrollMode = OVER_SCROLL_NEVER
+        }
+    }
+}
