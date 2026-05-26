d781fefd1918cbc511f2cc8ba0914745851ef9b8
barq <barqaday@gmail.com>
2026-05-26 03:59:27 +0300

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,26 +1,22 @@
-import js from "@eslint/js";
-import globals from "globals";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
-import tseslint from "typescript-eslint";
+import { CapacitorConfig } from '@capacitor/cli';
 
-export default tseslint.config(
-  { ignores: ["dist"] },
-  {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ["**/*.{ts,tsx}"],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+const config: CapacitorConfig = {
+  appId: 'com.drdshati.app',
+  appName: 'دردشاتي',
+  webDir: 'dist',
+  bundledWebRuntime: false,
+  plugins: {
+    Keyboard: {
+      resize: 'none',
     },
-    plugins: {
-      "react-hooks": reactHooks,
-      "react-refresh": reactRefresh,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
-      "@typescript-eslint/no-unused-vars": "off",
-    },
+    VoiceRecorder: {
+      // هذا السطر يجبر البلاگن يتسجل بالاندرويد
+    }
   },
-);
+  android: {
+    allowMixedContent: true,
+    backgroundColor: '#FFFFFF'
+  }
+};
+
+export default config;
