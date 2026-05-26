d30e56289418b562db0869a1fb56f3ac95d93022
barq <barqaday@gmail.com>
2026-05-26 03:13:43 +0300

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,58 +1,25 @@
-import { CapacitorConfig } from "@capacitor/cli";
+import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: "com.drdshati.app",
-  appName: "دردشة",
-  webDir: "dist",
-  server: {
-    androidScheme: "https",
-    cleartext: true,
-  },
-  android: {
-    backgroundColor: "#FFFFFF",
-    allowMixedContent: true,
-    captureInput: true,
-    webContentsDebuggingEnabled: false,
-    mixedContentMode: "always_allow"
-  },
-  ios: {
-    backgroundColor: "#FFFFFF",
-    contentInset: "always",
-    scrollEnabled: true,
-    limitsNavigationsToAppBoundDomains: true
-  },
+  appId: 'com.drdshati.app',
+  appName: 'دردشاتي',
+  webDir: 'dist',
+  bundledWebRuntime: false,
   plugins: {
-    CapacitorHttp: {
-      enabled: true
-    },
-    GoogleAuth: {
-      scopes: ["profile", "email"],
-      androidClientId: "159537926588-dtnce28t16apvq1calgdvjj1u1t95iq3.apps.googleusercontent.com",
-      serverClientId: "159537926588-dtnce28t16apvq1calgdvjj1u1t95iq3.apps.googleusercontent.com",
-      forceCodeForRefreshToken: true,
-    },
-    StatusBar: {
-      style: "dark",
-      backgroundColor: "#FFFFFF",
-      overlaysWebView: false,
-      androidOverlaysWebView: false
-    },
     Keyboard: {
-      resize: "none",
-      style: "dark",
-      resizeOnFullScreen: true,
+      resize: 'none', // مهم: يمنع التطبيق يتمطط مع الكيبورد
+      style: 'dark',
+      resizeOnFullScreen: true
     },
     SplashScreen: {
-      launchShowDuration: 0,
-      launchAutoHide: true,
-      backgroundColor: "#FFFFFF",
-      androidSplashResourceName: "splash",
-      androidScaleType: "CENTER_CROP",
-      showSpinner: false,
-      splashFullScreen: false,
-      splashImmersive: false,
+      launchShowDuration: 0
     }
   },
+  android: {
+    allowMixedContent: true,
+    captureInput: true,
+    webContentsDebuggingEnabled: false
+  }
 };
 
 export default config;
